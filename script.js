f4b543ff863f0cef7275223fcd90f6b447168c1e
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-04-20 15:32:49 +0100

    added handlebars functionality for featured products

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 fetch('template.hbs')
     .then(data => data.text())
     .then(data => {
-
         const template = Handlebars.compile(data)
-        const input =
-        {
-            name: "Headphones",
-            desc: "a pair of headphones",
-            price: 45.99,
-            originalPrice: 75,
-            discount: 45,
-            image: "https://cdn.shopify.com/s/files/1/1634/8185/products/akg_aviation_av100_anr_headset_01_270x335_crop_top.png?v=1480672940"
-        }
-        const html = template(input)
-        document.querySelector('.featured-gallery').innerHTML = html
+
+        fetch('items.json')
+            .then(response => response.json())
+            .then(response => {
+                const html = template(response)
+                document.querySelector('.featured-gallery').innerHTML = html
+            })
     })
